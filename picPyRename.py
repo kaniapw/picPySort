1f9aa7d56e9d27e96a0d4304cc9eac098ab6718d
kaniapw <kaniapw@gmail.com>
2017-08-02 14:26:38 +0200

    Adding picPyRename, tool for renaming files basing on EXIF or creation time

---
+++ picPyRename.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import exifread
+import datetime
+
+#const
+PICTURES_SOURCE = "c:\\art\\"
+PRINT_DEBUG = True
+
+
+def main(argv):
+    picCount = 0
+
+    for root, subDirs, files in os.walk(PICTURES_SOURCE):
+        for fileName in files:
+            fileExtension = fileName[-4:].lower()
+
+            if fileExtension in {".jpg", ".mp4", ".avi", ".3gp"}:
+                picCount = picCount + 1
+
+                if PRINT_DEBUG:
+                    print(root + "\\" + fileName)
+
+                fileHandle = open(root + "\\" + fileName, 'rb')
+
+                picEXIF = exifread.process_file(fileHandle, details=False)
+
+                if 'Image DateTime' in picEXIF:
+                    newFileName = str(picEXIF['Image DateTime'])
+                elif 'EXIF DateTimeOriginal' in picEXIF:
+                    newFileName = str(picEXIF['EXIF DateTimeOriginal'])
+                elif 'EXIF DateTimeDigitized' in picEXIF:
+                    newFileName = str(picEXIF['EXIF DateTimeDigitized'])
+                else:
+#                    newFileName = time.strftime("%Y %m %d %H %M %S", os.path.getmtime(root + "\\" + fileName))
+                    newFileName = datetime.datetime.fromtimestamp(os.path.getmtime(root + "\\" + fileName)).strftime("%Y %m %d %H %M %S")
+
+                fileHandle.close()
+
+                newFileName = newFileName.replace('/', '')
+                newFileName = newFileName.replace(':', '')
+                newFileName = newFileName.replace(' ', '')
+
+                if PRINT_DEBUG:
+                    print("File: " + root + "\\" + newFileName + fileExtension)
+                    print("New file name: " + root + "\\" + newFileName + fileExtension)
+
+                try:
+                    os.rename(root + "\\" + fileName, root + "\\" + newFileName + fileExtension)
+                except FileExistsError:
+                    print("File :" + root + "\\" + newFileName + fileExtension + " already exists!" )
+
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv)
