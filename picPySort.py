399146bd36655cb1a582a5df43ca80522c096090
kaniapw <kaniapw@gmail.com>
2018-12-28 11:58:47 +0100

    Moving to Here reverse geocoding engine

--- picPySort.py
+++ picPySort.py
@@ -18,7 +18,6 @@ HOME_AREA = 35
 OTHER_AREA = 15
 REMOVE_FROM_MODEL = ["/", "<", ">"]
 
-GEOCODE_URL = 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?app_id=DWfPK1hjYRBLdNACkeQH&app_code=f90Cij5MJ38YIs_s0lbqoA&mode=retrieveAreas&prox='
 #GEOCODE_URL = 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?app_id={YOUR_APP_ID}&app_code={YOUR_APP_CODE}&mode=retrieveAreas&prox='
 HTTP_OK = 200
 PRINT_DEBUG = False
